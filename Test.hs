297e419540363d9576a0d3f0f2c50077beabf110
Henrique Ferreiro <hferreiro@udc.es>
2013-05-28 14:32:56 +0200

    Improve argument parsing

--- Test.hs
+++ Test.hs
@@ -1,8 +1,11 @@
 module Main where
 
+import Control.Monad ( when )
 import Text.PrettyPrint ( render )
 import System.IO ( stdin, hGetContents )
+import System.Directory ( doesFileExist )
 import System.Environment ( getArgs, getProgName )
+import System.FilePath ( (<.>), dropExtension )
 
 import Data.Class ( addConstrs )
 import Language.SurfaceOCL.OCL
@@ -11,31 +14,14 @@ import Text.WSDL.ClassParser ( wsdl2Class )
 
 type ParseFun a = [Token] -> Err a
 
-type Verbosity = Int
+runFile :: ParseFun OCLfile -> (FilePath, FilePath) -> IO ()
+runFile p (w, o)
+  = do wc <- readFile w
+       oc <- readFile o
+       run p wc oc
 
-putStrV :: Verbosity -> String -> IO ()
-putStrV v s = if v > 0 then putStrLn s else return ()
-
-runFile :: (ErlangPropList a, {-Print a,-} Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
-runFile v p f = putStrLn f >> readFile f >>= run v p
-
-runFiles :: ParseFun OCLfile -> FilePath -> FilePath -> IO ()
-runFiles p w o = do
-  wc <- readFile w
-  oc <- readFile o
-  runs p wc oc
-
-run :: (ErlangPropList a, {-Print a,-} Show a) => Verbosity -> ParseFun a -> String -> IO ()
-run v p s = let ts = myLexer s in
-              case p ts of
-                Bad s    -> do putStrLn "Parse Failed."
-                               putStrV v "Tokens:"
-                               putStrV v $ show ts
-                               putStrLn s
-                Ok  tree -> do showTree v tree
-
-runs :: ParseFun OCLfile -> String -> String -> IO ()
-runs p wc oc
+run :: ParseFun OCLfile -> String -> String -> IO ()
+run p wc oc
   = let ts = myLexer oc
         cl = wsdl2Class wc
     in case p ts of
@@ -43,19 +29,19 @@ runs p wc oc
                            cl'= addConstrs cl m
                        putStrLn $ render (proplist [cl']) ++ "."
 
-showTree :: (ErlangPropList a, Show a{-, Print a-}) => Verbosity -> a -> IO ()
-showTree v tree =
-  case v of
-    1 -> do putStrV v $ "[Abstract Syntax]\n" ++ show tree
-            --putStrV v $ "[Linearized tree]\n" ++ printTree tree
-    2 -> do putStrLn (render $ proplist tree)
-    _ -> return ()
+getFiles :: FilePath -> IO (FilePath, FilePath)
+getFiles f
+  = do b <- doesFileExist w
+       when (not b) (errorF w)
+       b <- doesFileExist o
+       when (not b) (errorF o)
+       return (w, o)
+    where f' = dropExtension f
+          (w, o) = (f' <.> "wsdl", f' <.> "ocl")
+          errorF f = error $ "File '" ++ f ++ "' does not exist"
 
 main :: IO ()
 main = do args <- getArgs
           case args of
-            [] -> hGetContents stdin >>= run 0 pOCLfile
-            "-v":fs -> mapM_ (runFile 1 pOCLfile) fs
-            "-e":fs -> mapM_ (runFile 2 pOCLfile) fs
-            ["-w",w,o] -> runFiles pOCLfile w o
-            fs -> mapM_ (runFile 0 pOCLfile) fs
+            []  -> error "Usage: Test file[.wsdl/.ocl]"
+            f:_ -> getFiles f >>= runFile pOCLfile
